fb917ec855373bd9210216504654d18549156ca3
Cedric Brancourt <cedric.brancourt@gmail.com>
2015-05-12 12:23:30 +0200

    chore(initialisation): add init.rb

---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :issue_workflow_button do
+  name 'Issue workflow button'
+  author 'Cedric Brancourt @ Synbioz'
+  description 'Add button(s) for available workflow transitions on issue view'
+  version '0.0.1'
+end
