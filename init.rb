deffda115a28a2461bb0fefeb11d4875fa74210b
Cedric Brancourt <cedric.brancourt@gmail.com>
2015-05-12 16:44:51 +0200

    refacto(naming): pluralized plugin name

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
-require_dependency 'iwb_hook_listeners'
+require_dependency 'issue_workflow_buttons/view_hook_listeners'
 
-Redmine::Plugin.register :issue_workflow_button do
-  name 'Issue workflow button'
+Redmine::Plugin.register :issue_workflow_buttons do
+  name 'Issue workflow buttons'
   author 'Cedric Brancourt @ Synbioz'
   description 'Add button(s) for available workflow transitions on issue view'
   version '0.0.1'
--- lib/issue_workflow_buttons/view_hook_listeners.rb
+++ lib/issue_workflow_buttons/view_hook_listeners.rb
@@ -1,3 +1,3 @@
-class IwbHookListeners < Redmine::Hook::ViewListener
+class IssueWorkflowButtons::ViewHookListeners < Redmine::Hook::ViewListener
   render_on(:view_issues_show_details_bottom, partial: 'issues/workflow_buttons')
 end
